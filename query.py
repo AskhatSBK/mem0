6be23027c7a4e6ee34173dbb59cfe4d4c6fae6ad
AskhatSBK <askhat.sabitkhanov@mail.ru>
2025-07-14 11:00:30 +0500

    config deploy

---
+++ query.py
@@ -0,0 +1,23 @@
+import os
+from mem0 import Memory
+
+config = {
+    "llm": {
+        "provider": "vllm",
+        "config": {
+            "model": "unsloth/gemma-3-1b-it",
+            "vllm_base_url": "http://localhost:8000/v1",
+            "temperature": 0.1,
+            "max_tokens": 2000,
+        }
+    },
+        "embedder": {
+        "provider": "ollama",
+        "config": {
+            "model": "bge-m3:567m"
+        }
+    }
+}
+
+m = Memory.from_config(config)
+m = Memory()
